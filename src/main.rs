10ae83e4baa15a5206d1cd6cef498d3d248a3dcb
crypt0box <pray.happy76@gmail.com>
2022-08-11 14:23:35 +0900

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod generics;
+mod lifetime;
 mod vars;
 
 fn main() {
     // println!("Hello, world!");
     // vars::run();
-    generics::run();
+    // generics::run();
+    lifetime::run();
     // vars::sub_a::func_a();
     // vars::sub_b::func_b();
 }
